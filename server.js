d2de0077bc7647700f9f37718146c409c6182a0b
Poria <pouriyaporgoly@gmail.com>
2025-07-01 18:01:58 +0330

    fixing

--- server.js
+++ server.js
@@ -37,6 +37,9 @@ function requireAuth(req, res, next) {
 }
 
 // Routes
+app.get('/', (res, req) => {
+    res.redirect('/users')
+});
 app.get('/register', (req, res) => res.render('register', { error: null }));
 app.post('/register', async (req, res) => {
     try {
